f6e8ce74d920368f611bad5f844e14722e1aca7e
Alessandro Averna <alessandro.averna.1994@gmail.com>
2024-06-13 10:30:41 +0200

    added DI

---
+++ ProcedureManagement/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
