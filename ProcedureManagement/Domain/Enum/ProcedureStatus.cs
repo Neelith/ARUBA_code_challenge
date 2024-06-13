dd9c13120e5960c0c71d60f6eac3d0e650872099
Alessandro Averna <alessandro.averna.1994@gmail.com>
2024-06-13 10:11:33 +0200

    added procedure status

---
+++ ProcedureManagement/Domain/Enum/ProcedureStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enum
+{
+    public enum ProcedureStatus
+    {
+        Created = 0,
+        InProgress = 1,
+        Approved = 2,
+        Rejected = 3
+    }
+}
