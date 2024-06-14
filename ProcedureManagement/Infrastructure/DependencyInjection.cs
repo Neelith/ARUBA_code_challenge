1e912596e85a2b24d69b17a8d3dae92053ee8118
Alessandro Averna <alessandro.averna.1994@gmail.com>
2024-06-14 17:14:23 +0200

    added connection string

--- ProcedureManagement/Infrastructure/DependencyInjection.cs
+++ ProcedureManagement/Infrastructure/DependencyInjection.cs
@@ -17,7 +17,7 @@ namespace Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var connectionString = configuration.GetConnectionString("ProceduresDb");
 
             ArgumentNullException.ThrowIfNull(connectionString);
 
