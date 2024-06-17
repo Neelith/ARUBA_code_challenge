30853925c073bb7ec83a656c364701e28ec69aba
Alessandro Averna <alessandro.averna.1994@gmail.com>
2024-06-17 15:06:36 +0200

    added migrations, fixed logging on db

---
+++ ProcedureManagement/Infrastructure/Data/Migrations/20240617124400_InitialSetup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "procedures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrentProcedureStatus = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_procedures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "attachments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProcedureId = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_attachments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_attachments_procedures_ProcedureId",
+                        column: x => x.ProcedureId,
+                        principalTable: "procedures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "procedureStatusHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProcedureId = table.Column<int>(type: "int", nullable: false),
+                    ProcedureStatus = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_procedureStatusHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_procedureStatusHistory_procedures_ProcedureId",
+                        column: x => x.ProcedureId,
+                        principalTable: "procedures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_attachments_ProcedureId",
+                table: "attachments",
+                column: "ProcedureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_procedureStatusHistory_ProcedureId",
+                table: "procedureStatusHistory",
+                column: "ProcedureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "attachments");
+
+            migrationBuilder.DropTable(
+                name: "procedureStatusHistory");
+
+            migrationBuilder.DropTable(
+                name: "procedures");
+        }
+    }
+}
---
+++ ProcedureManagement/Infrastructure/Data/Migrations/20240617124518_LogTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string createLogTableIfNotExists = @"
+            IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[logs]') AND type in (N'U'))
+                CREATE TABLE [dbo].[logs] (
+
+                   [Id] int IDENTITY(1,1) NOT NULL,
+                   [Message] nvarchar(max) NULL,
+                   [MessageTemplate] nvarchar(max) NULL,
+                   [Level] nvarchar(128) NULL,
+                   [TimeStamp] datetime NOT NULL,
+                   [Exception] nvarchar(max) NULL,
+                   [Properties] nvarchar(max) NULL
+
+                   CONSTRAINT [PK_Logs] PRIMARY KEY CLUSTERED ([Id] ASC)
+                );";
+            migrationBuilder.Sql(createLogTableIfNotExists);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string deleteLogTableIfExists = @"
+                IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[logs]') AND type in (N'U'))
+                DROP TABLE [dbo].[logs]";
+            migrationBuilder.Sql(deleteLogTableIfExists);
+        }
+    }
+}
